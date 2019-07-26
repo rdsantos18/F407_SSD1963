5842aa9ec9dc596cf012f92a6758d9666834610f
rdsantos18 <rdsantos@uol.com.br>
2019-07-25 23:18:10 -0300

    Update lvgl

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,7 +24,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "lvgl/lvgl.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
